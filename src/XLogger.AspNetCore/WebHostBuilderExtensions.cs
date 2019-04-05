6d9ca998892c1da464887ff3ce673b7722c15d7e
André Secco <andreluizsecco@yahoo.com.br>
2019-04-05 17:59:34 -0300

    Some implementations

--- src/XLogger.AspNetCore/WebHostBuilderExtensions.cs
+++ src/XLogger.AspNetCore/WebHostBuilderExtensions.cs
@@ -1,19 +1,12 @@
 using System;
-using Microsoft.Extensions.Logging;
 using XLogger;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.AspNetCore.Hosting
 {
     public static class WebHostBuilderExtensions
     {
-        public static IWebHostBuilder UseXLogger(this IWebHostBuilder builder, Action<ILoggerHub> loggers)
-        {
-            return builder.ConfigureLogging((hostingContext, logging) =>
-            {
-                logging.ClearProviders();
-                var hub = new LoggerHub(logging.Services);
-                loggers.Invoke(hub);
-            });
-        }
+        public static IWebHostBuilder UseXLogger(this IWebHostBuilder builder, Action<ILoggerHub> loggerHubOptions) =>
+            builder.ConfigureLogging((hostingContext, logging) => logging.RegisterLoggerHub(loggerHubOptions));
     }
-}
+}   
