c1d2af3a8a3d64f8dd68fb28d978582324252c6b
André Secco <andreluizsecco@yahoo.com.br>
2019-04-10 15:28:10 -0300

    Added nuget package info and documentation comments

--- src/XLogger.AspNetCore/WebHostBuilderExtensions.cs
+++ src/XLogger.AspNetCore/WebHostBuilderExtensions.cs
@@ -5,6 +5,12 @@ namespace XLogger
 {
     public static class WebHostBuilderExtensions
     {
+        /// <summary>
+        /// Sets XLogger as the logging provider.
+        /// </summary>
+        /// <param name="builder">The web host builder to configure.</param>
+        /// <param name="loggerHubOptions">The logger hub options and preferences.</param>
+        /// <returns>The web host builder.</returns>
         public static IWebHostBuilder UseXLogger(this IWebHostBuilder builder, Action<ILoggerHub> loggerHubOptions) =>
             builder.ConfigureLogging((hostingContext, logging) => logging.RegisterLoggerHub(loggerHubOptions));
     }
