a3daef055be4533a2938b80db98c843d58c99a95
André Secco <andreluizsecco@yahoo.com.br>
2019-09-24 16:24:32 -0300

    Add support for .NET Core 3.0

--- src/XLogger.AspNetCore/WebHostBuilderExtensions.cs
+++ src/XLogger.AspNetCore/WebHostBuilderExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace XLogger
 {
@@ -12,6 +13,15 @@ namespace XLogger
         /// <param name="loggerHubOptions">The logger hub options and preferences.</param>
         /// <returns>The web host builder.</returns>
         public static IWebHostBuilder UseXLogger(this IWebHostBuilder builder, Action<ILoggerHub> loggerHubOptions) =>
+            builder.ConfigureLogging((hostingContext, logging) => logging.RegisterLoggerHub(loggerHubOptions));
+
+        /// <summary>
+        /// Sets XLogger as the logging provider.
+        /// </summary>
+        /// <param name="builder">The host builder to configure.</param>
+        /// <param name="loggerHubOptions">The logger hub options and preferences.</param>
+        /// <returns>The host builder.</returns>
+        public static IHostBuilder UseXLogger(this IHostBuilder builder, Action<ILoggerHub> loggerHubOptions) =>
             builder.ConfigureLogging((hostingContext, logging) => logging.RegisterLoggerHub(loggerHubOptions));
     }
 }   
